3284341478a9beafd219c3fcf0f2beea7a957a7a
saturn <45328849+saturnglave@users.noreply.github.com>
2019-11-20 16:26:05 +0900

    rockstar問題追加

---
+++ picoCTF2019/rock_pico.py
@@ -0,0 +1,40 @@
+Pico = 19
+my_mind = 6
+It = 6
+This = my_mind * Pico
+my_flag = 35
+put This into my_flag
+put my_flag into Pico
+shout Pico
+shout Pico
+shout Pico
+my_song = 9
+put Pico into This
+This -= 1, down, down
+put This into CTF
+shout CTF
+my_lyric = False
+my_lyric = This - my_song
+my_lyric -= 1, down, down
+shout my_lyric
+This = my_lyric
+my_lyric = my_song + This
+my_lyric -= 1
+shout my_lyric
+my_lyric += 1, up ,up
+shout my_lyric
+shout Pico
+shout It
+Pico_CTF = 3
+security = important
+Fun = 3
+Pico_CTF = security + fun
+Fun += 1
+shout fun * Pico_CTF
+put fun * Pico_CTF into my_song
+build Fun up
+shout it
+shout it
+build Fun up, up
+shout it
+shout Pico
